c3866c820976e3208c1946203325e8ae72b7e6f6
Valerio Piselli <vpiselli@wss.com.do>
2022-03-22 16:21:35 -0400

    Lesson 11: Suspense (Experimental)

--- vite-example/src/components/HelloWorld.oldway.vue
+++ vite-example/src/components/HelloWorld.oldway.vue
@@ -6,40 +6,17 @@
 		<p v-else="">There was an error...</p>
 	</div>
 	<p v-else="">Loading...</p>
-
-	<h2>Suspense</h2>
-
-	<div v-if="suspenseError">{{ suspenseError }}</div>
-	<Suspense v-else>
-		<template #default>
-			<DadJoke />
-		</template>
-
-		<template #fallback>
-			Loading...
-		</template>
-	</Suspense>
 </template>
 
 <script>
 import axios from 'axios'
-import { onErrorCaptured, ref } from 'vue'
-import DadJoke from './DadJoke.vue'
+import { ref } from 'vue'
 
 export default {
-	components: {
-		DadJoke
-	},
-
 	setup() {
 		const dadJoke = ref(null)
 		const isLoading = ref(true)
 		const isError = ref(false)
-		const suspenseError = ref(null)
-
-		onErrorCaptured(e => {
-			suspenseError.value = e.message
-		})
 
 		axios.get('https://icanhazdadjoke.com/', {
 			headers: {
@@ -59,7 +36,6 @@ export default {
 			dadJoke,
 			isLoading,
 			isError,
-			suspenseError
 		}
 	}
 }
