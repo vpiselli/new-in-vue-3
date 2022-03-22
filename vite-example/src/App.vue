4b0e9edba318363a70381f229f44bec0088b2781
Valerio Piselli <vpiselli@wss.com.do>
2022-03-22 11:42:31 -0400

    Lesson 8: Multiple V-Models

---
+++ vite-example/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<InviteForm v-model:name="inviteName" v-model:email="inviteEmail" />
+	<div>{{ inviteName }}</div>
+	<div>{{ inviteEmail }}</div>
+</template>
+
+<script>
+import { ref } from "vue"
+import InviteForm from './components/InviteForm.vue';
+
+export default {
+	name: 'App',
+
+	components: {
+		InviteForm
+	},
+
+	setup() {
+		const inviteName = ref('')
+		const inviteEmail = ref('')
+
+		return {
+			inviteName,
+			inviteEmail
+		}
+	}
+}
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ vite-example/src/components/InviteForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form action="">
+        <div>
+            <label for="name">Name</label>
+            <input
+                type="text"
+                :value="name"
+                @input="event => $emit('update:name', event.target.value)"
+            >
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input
+                type="text"
+                :value="email"
+                @input="event => $emit('update:email', event.target.value)"
+            >
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    props: [ 'name', 'email' ]
+}
+</script>
